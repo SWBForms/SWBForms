b0a906e134b499db9257f5c321cea94a05f3190e
Hasdai Pacheco <haxdai@gmail.com>
2017-01-09 18:17:40 -0600

    Added geoJSON test

--- src/main/webapp/app/js/controllers/maps.controller.js
+++ src/main/webapp/app/js/controllers/maps.controller.js
@@ -6,9 +6,20 @@
     .controller('MapsCtrl', MapsCtrl);
 
     MapsCtrl.$inject = ['$scope', '$http'];
-    function MapsCtrl($scope, $FileManager) {
+    function MapsCtrl($scope, $http) {
       //Invoke dataviz functions here
-      dataviz.mapsFactory.createMap("map1");
+
+      let request = $http({
+        url: "/app/mockdata/geo/PM_15_municipio.geojson",
+        method: "GET"
+      }).then((res) => {
+        console.log(res.data);
+        let mp = dataviz.mapsFactory.createMap("map1");
+        dataviz.mapsFactory.addGeoJSONLayer(mp, res.data);
+      });
+
+      //dataviz.mapsFactory.addGeoJSONLayer(mp, data);
+
       //console.log(dataviz.maps.createMap(););
     };
 })();
