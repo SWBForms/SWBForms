4b4d280cb06c79977064ff0b7c7874aa1c442f73
Hasdai Pacheco <haxdai@gmail.com>
2017-01-10 11:06:13 -0600

    Plotting geoJSON information

--- src/main/webapp/app/js/controllers/maps.controller.js
+++ src/main/webapp/app/js/controllers/maps.controller.js
@@ -8,13 +8,20 @@
     MapsCtrl.$inject = ['$scope', '$http'];
     function MapsCtrl($scope, $http) {
       //Invoke dataviz functions here
+      let mp = dataviz.mapsFactory.createMap("map1");
 
-      let request = $http({
+      $http({
         url: "/app/mockdata/geo/PM_15_municipio.geojson",
         method: "GET"
       }).then((res) => {
         console.log(res.data);
-        let mp = dataviz.mapsFactory.createMap("map1");
+        dataviz.mapsFactory.addGeoJSONLayer(mp, res.data);
+      });
+
+      $http({
+        url: "/app/mockdata/geo/PM_15_puntos_complem.geojson",
+        method: "GET"
+      }).then((res) => {
         dataviz.mapsFactory.addGeoJSONLayer(mp, res.data);
       });
 
