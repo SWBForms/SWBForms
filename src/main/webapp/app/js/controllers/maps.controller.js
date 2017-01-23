5c908a694140ac966744589c62031b705c92749e
Donnaji <tcidonita@hotmail.com>
2017-01-23 12:18:50 -0600

    Update maps.controller.js

--- src/main/webapp/app/js/controllers/maps.controller.js
+++ src/main/webapp/app/js/controllers/maps.controller.js
@@ -9,27 +9,6 @@
     function MapsCtrl($scope, $http) {
       //Invoke dataviz functions here
       let mp = dataviz.mapsFactory.createMap("map1",ENGINE_LEAFLET );
-      let mark1 = dataviz.mapsFactory.addMarker(mp, [25.789, -109.004]);
-      let mark2 = dataviz.mapsFactory.addMarker(mp, [25.761, -108.967]);
-
-      $http({
-        url: "/app/mockdata/geo/PM_15_municipio.geojson",
-        method: "GET"
-      }).then((res) => {
-        console.log(res.data);
-        dataviz.mapsFactory.addGeoJSONLayer(mp, res.data);
-      });
-
-      $http({
-        url: "/app/mockdata/geo/coordenadas.json",
-        method: "GET"
-      }).then((res) => {
-        console.log(res.data);
-        dataviz.mapsFactory.makePolygon(mp, res.data);
-      });
-      let popUp = dataviz.mapsFactory.addPopUp(mark1, "The most beautiful place in this town", true);
-      let popUp2 = dataviz.mapsFactory.addPopUp(mark2, "adventure", false);
-      let mark3 = dataviz.mapsFactory.addMarker(mp, [25.761, -110.967]);
 
       $http({
         url: "/app/mockdata/geo/PM_15_municipio.geojson",
