0b2efc155f40f62b4344c878c0b13cd4ce73b6e4
Karlos J <juancarlos.proteco@gmail.com>
2017-03-19 14:04:09 -0600

    solved issue 18 and issue 19

--- src/main/webapp/app/js/controllers/dashboards/editdashboard.controller.js
+++ src/main/webapp/app/js/controllers/dashboards/editdashboard.controller.js
@@ -11,11 +11,14 @@
 
     let deletedWidgets = [];
 
-    let newDashboard = {
-      name: "",
-      widgets: []
-    }
+    $scope.gridsterOptions = {
+      columns: 6,
+      pushing: true,
+      margins: [5, 5],
+      mobileModeEnabled: true
+    };
 
+    //get dashboards and widgets from database
     if ($stateParams.id && $stateParams.id.length) {
       $Datasource.getObject($stateParams.id, "Dashboard").then(
         dashboard => {
@@ -37,94 +40,107 @@
       $scope.pageTitle = "Editar dashboard";
 
     } else {
+
+      let newDashboard = {
+        name: "",
+        widgets: []
+      }
+
       $scope.pageTitle = "Nuevo dashboard";
       $scope.dashboard = newDashboard;
     }
 
     if ($stateParams.mode == "view") {
 
-      $scope.gridsterOptions = {
-        columns: 6,
-        pushing: true,
-        margins: [5, 5],
-        mobileModeEnabled: true,
-        draggable: false,
-        resizable: false
-      };
-
+      $scope.gridsterOptions.draggable = false;
+      $scope.gridsterOptions.resizable = false;
       $scope.formVisible = false;
       $scope.desactiveClass = "desactive-element";
 
     } else {
 
-      $scope.gridsterOptions = {
-        columns: 6,
-        pushing: true,
-        margins: [5, 5],
-        mobileModeEnabled: true,
-        draggable: {
-          enabled: true,
-          handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw']
-        },
-        resizable: {
-          enabled: true
-        }
+      $scope.gridsterOptions.draggable = {
+        enabled: true,
+        handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw']
       };
-
+      $scope.gridsterOptions.resizable = {
+        enabled: true
+      };
       $scope.formVisible = true;
     }
 
+    $scope.returnPage = function($event) {
+      $state.go('dashboard.dashboards',{});
+    }
+
     $scope.addWidget = function() {
-      let itemId = Date.now();
-      $scope.dashboard.widgets.push({
-        _id: itemId,
-        name: 'widget',
-        content: '<div class="gridster-angular-content-item"></div>',
-        sizeX: 1,
-        sizeY: 1
-      });
+      if ($scope.formVisible) {
+        $.get('templates/dashboard/widgetEditForm.html',template => {
+          bootbox.confirm(template, result => {
+            if (result) {
+              let data = {};
+
+              //get form data
+              data.name = $('#widgetFormName').val();
+              data.type = $('#widgetFormType').val();
+
+              if (data.name.length < 1) {
+                data.name = 'widget';
+              };
+
+              $scope.$apply(() => {
+                let itemId = Date.now();
+                $scope.dashboard.widgets.push({
+                  _id: itemId,
+                  name: data.name,
+                  type: data.type,
+                  content: '<div class="gridster-angular-content-item"></div>',
+                  sizeX: 1,
+                  sizeY: 1
+                });
+              });
+            };
+          });
+        });
+      };
     };
 
     $scope.editWidget = function($event) {
-      let widgetId = $event.target.id;
-      $.get('templates/dashboard/widgetEditForm.html', function(data) {
-        bootbox.confirm(data, function(result) {
-          if(result) {
+      if ($scope.formVisible) {
+        let widgetId = $event.target.id;
+        $.get('templates/dashboard/widgetEditForm.html', template => {
+          bootbox.confirm(template, result => {
+            if(result) {
 
-            var data = {};
+              let data = {};
 
-            $("#widgetForm input").each(function(index, value ) {
-              data[$(value).attr('name')] = $(value).val();
-            });
+              //get form data
+              data.name = $('#widgetFormName').val();
+              data.type = $('#widgetFormType').val();
 
-            $scope.$apply(function() {
-              for(var i=0 ; i < $scope.dashboard.widgets.length; i++) {
-                if($scope.dashboard.widgets[i]._id == widgetId) {
-                  if (data['name'].length) {
-                    $scope.dashboard.widgets[i].name = data['name'];
+              $scope.$apply(() => {
+                for(var i=0 ; i < $scope.dashboard.widgets.length; i++) {
+                  if($scope.dashboard.widgets[i]._id == widgetId) {
+                    if (data['name'].length) {
+                      $scope.dashboard.widgets[i].name = data['name'];
+                    };
+                    if (data['type'].length) {
+                      $scope.dashboard.widgets[i].type = data['type'];
+                    };
                   };
-                  if (data['ancho'].length && $.isNumeric(data['ancho'])) {
-                    $scope.dashboard.widgets[i].sizeX = data['ancho'];
-                  };
-                  if (data['alto'].length && $.isNumeric(data['alto'])) {
-                    $scope.dashboard.widgets[i].sizeY = data['alto'];
-                  };
-                  if (data['row'].length && $.isNumeric(data['row'])) {
-                    $scope.dashboard.widgets[i].row = data['row'];
-                  };
-                  if (data['col'].length && $.isNumeric(data['col'])) {
-                    $scope.dashboard.widgets[i].col = data['col'];
-                  };
-                }
-              }
-            });
-
-          }
+                };
+              });
+            };
+          });
         });
-      });
+      };
     };
 
     $scope.save = function() {
+
+      if (!$scope.formVisible) {
+        return;
+      };
 
       let dashboard = {};
       let widgetsObject = {};
@@ -135,36 +151,41 @@
       dashboard.name = $scope.dashboard.name;
       dashboard.widgets = {};
 
-      for (var i = 0; i < deletedWidgets.length; i++) {
-        let widget ={};
-        widget._id = deletedWidgets[i]._id;
-        $Datasource.removeObject(widget._id, "Widget")
-        .catch(function(error) {});
-      };
-
       for(var i=0 ; i < $scope.dashboard.widgets.length; i++) {
         let widget = {};
 
         widget._id = $scope.dashboard.widgets[i]._id;
         widget.name = $scope.dashboard.widgets[i].name;
-        widget.type = "undefined"
+        widget.type = $scope.dashboard.widgets[i].type
         widget.col = $scope.dashboard.widgets[i].col;
         widget.row = $scope.dashboard.widgets[i].row;
         widget.sizeX = $scope.dashboard.widgets[i].sizeX;
         widget.sizeY = $scope.dashboard.widgets[i].sizeY;
 
         $Datasource.getObject(widget._id, "Widget")
-        .then(function(result){
-          saveDashboard({"_id": widget._id, "name": widget.name });
+        .then(result => {
+          $Datasource.updateObject(widget,"Widget")
+          .then(result => {
+            saveDashboard({"_id": widget._id, "name": widget.name });
+          });
         })
-        .catch(function(error){
+        .catch(error => {
           delete widget['_id'];
           $Datasource.addObject(widget,"Widget")
-          .then(function(result) {
+          .then(result => {
             saveDashboard({"_id":result.data.response.data._id, "name": result.data.response.data.name });
           });
         });
       };
+
+      function deleteWidgets() {
+        //delete widgets from the database
+        for (var i = 0; i < deletedWidgets.length; i++) {
+          let widget ={};
+          widget._id = deletedWidgets[i]._id;
+          $Datasource.removeObject(widget._id, "Widget").catch(error => {});
+        };
+      }
 
       function saveDashboard(widget) {
         widgetsObject[indice] = widget;
@@ -175,7 +196,8 @@
             $Datasource.updateObject(dashboard,"Dashboard")
             .then(response => {
               $state.go('dashboard.dashboards',{})
-            }).catch(error => {
+            })
+            .catch(error => {
               console.log(error);
             })
           } else {
@@ -185,31 +207,34 @@
             })
             .catch(error => {
               console.log(error);
-            })
+            });
           }
         };
       };
-
     }
 
     $scope.deleteWidget = function($event) {
-      let widgetId = $event.target.id;
-      for(var i=0 ; i < $scope.dashboard.widgets.length; i++) {
-        if($scope.dashboard.widgets[i]._id == widgetId) {
-          deletedWidgets.push(widgetId);
-          $scope.dashboard.widgets.splice(i, 1);
-        }
-      }
-    }
+      if ($scope.formVisible) {
+        let widgetId = $event.target.id;
+        for(var i=0 ; i < $scope.dashboard.widgets.length; i++) {
+          if($scope.dashboard.widgets[i]._id == widgetId) {
+            deletedWidgets.push(widgetId);
+            $scope.dashboard.widgets.splice(i, 1);
+          };
+        };
+      };
+    };
 
     $scope.clear = function() {
+      if ($scope.formVisible) {
+        $.each($scope.dashboard.widgets, function(index, value) {
+          deletedWidgets.push(value._id);
+        });
 
-      $.each($scope.dashboard.widgets, function(index, value) {
-        deletedWidgets.push(value._id);
-      });
-
-      $scope.dashboard.widgets = [];
+        $scope.dashboard.widgets = [];
+      };
     };
+
   };
 
 })();
